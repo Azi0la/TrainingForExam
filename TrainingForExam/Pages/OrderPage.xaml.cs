933fb7770d6fbe0576438e883b2e215b873f7877
Aziola <aziola25@gmail.com>
2026-06-23 18:47:18 +0300

    Готово

--- TrainingForExam/Pages/OrderPage.xaml.cs
+++ TrainingForExam/Pages/OrderPage.xaml.cs
@@ -20,9 +20,44 @@ namespace TrainingForExam.Pages
     /// </summary>
     public partial class OrderPage : Page
     {
-        public OrderPage()
+        public List<Sessions> Sessions = Core.Context.Sessions.ToList();
+        private Haircuts curHair;
+        private Masters curMast;
+        private String curDate;
+        public OrderPage(Masters MastID, String Date, Haircuts HairID = null)
         {
             InitializeComponent();
+            curHair = HairID;
+            curMast = MastID;
+            curDate = Date;
+            if(curHair != null ) HairCutTB.Text = curHair.Name;
+            MasterTB.Text = curMast.Name;
+            TimeTB.Text = curDate;
+            
+
+        }
+
+        private void OrderBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Sessions S = Sessions.FirstOrDefault(p => p.Date == DateTime.Parse(TimeTB.Text) && p.ID_Master == curMast.ID);
+            S.ID_Haircut = curHair.ID;
+            S.Name = NameTB.Text;
+            S.Phone = PhoneTB.Text;
+            S.IsBooked = true;
+            Core.Context.SaveChanges();
+            MessageBox.Show("Регистрация прошла успешно!");
+            NavigationService.Navigate(new MainPage());
+
+        }
+
+        private void BackBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if(NavigationService.CanGoBack) NavigationService.GoBack();
+        }
+
+        private void NameTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(NameTB.Text) && !string.IsNullOrEmpty(PhoneTB.Text)) OrderBTN.IsEnabled = true;
         }
     }
 }
